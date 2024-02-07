26c1038b5b9b8f2a3e41c03f218e76f9165ee360
ngangi0195 <96700425+ngangi0195@users.noreply.github.com>
2024-02-07 10:41:49 -0500

    Add files via upload

--- HashMapTest.java
+++ HashMapTest.java
@@ -4,17 +4,21 @@ import java.io.*;
 import java.util.*;
 
 public class HashMapTest {
-	static HashMap<Integer, ActionItem> map = new HashMap<>();
+	static HashMap<Integer, ActionItem[]> map = new HashMap<>();
 	
 	final static String outputFilePath = "text.txt";
 	
 	public static void main(String [] args)
 	{
 		System.out.println("Hello world");
-		map.put(1, new ActionItem("Nidhin", "Urgent", 2, 5, 2024, "Doing work"));
-		map.put(2, new ActionItem("Sam", "Current", 2, 4, 2024, "Not doing work"));
-		map.put(3, new ActionItem("Tristan", "Awaiting", 2, 3, 2024, "Working"));
 		
+		ActionItem[] list = {new ActionItem("Nidhin", "Urgent", 2, 5, 2024, "Doing work"), new ActionItem("Sam", "Current", 2, 4, 2024, "Not doing work"), new ActionItem("Tristan", "Awaiting", 2, 3, 2024, "Working")};
+		
+		ActionItem[] list2 = {new ActionItem("Maddie", "Lost", 2, 6, 2024, "Playing Games"), new ActionItem("Lily", "Current", 2, 4, 2024, "Actively Working"), new ActionItem("Aiden", "Awaiting", 2, 3, 2024, "Chipotle")};
+
+		
+		map.put(1, list);
+		map.put(2, list2);
 		
 		File file = new File(outputFilePath);
 	
@@ -31,12 +35,12 @@ public class HashMapTest {
             bf = new FileWriter(outputFilePath); 
   
             // iterate map entries 
-            for (Map.Entry<Integer, ActionItem> entry : 
-                 map.entrySet()) { 
-  
-                // put key and value separated by a colon 
-                bf.write(entry.getKey() + ":"
-                         + entry.getValue().toString()); 
+            for (int i: map.keySet()) {
+            	bf.write("V" + i + ":\n");
+            	for(ActionItem item : map.get(i)) 
+            	{
+            		bf.write(item.toString() + "\n"); 
+            	}
             } 
   
             bf.flush(); 
