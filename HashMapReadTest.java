47a13397fdd937dd104b51a95333ff731687dac4
ngangi0195 <96700425+ngangi0195@users.noreply.github.com>
2024-02-14 10:45:01 -0500

    Add files via upload

---
+++ HashMapReadTest.java
@@ -0,0 +1,108 @@
+package dev;
+
+//Java program to reading 
+//text file to HashMap 
+
+import java.io.*; 
+import java.util.*;
+import java.util.Date;
+import java.time.*;
+
+public class HashMapReadTest { 
+	final static String filePath 
+		= "test.txt"; 
+	public static void main(String[] args) 
+	{ 
+		// read text file to HashMap 
+		Map<Integer, ActionItem[]> mapFromFile 
+			= HashMapFromTextFile(); 
+
+		// iterate over HashMap entries 
+		for (Map.Entry<Integer, ActionItem[]> entry : 
+			mapFromFile.entrySet()) { 
+			System.out.println(entry.getKey() + " : "
+							+ entry.getValue()); 
+		} 
+	} 
+
+	public static Map<Integer, ActionItem[]> HashMapFromTextFile() 
+	{ 
+
+		Map<Integer, ActionItem[]> map 
+			= new HashMap<Integer, ActionItem[]>(); 
+		BufferedReader br = null; 
+
+		try { 
+
+			// create file object 
+			File file = new File(filePath); 
+
+			// create BufferedReader object from the File 
+			br = new BufferedReader(new FileReader(file)); 
+
+			String line = null; 
+
+			// read file line by line 
+			while ((line = br.readLine()) != null) { 
+
+				// split the line by : 
+				String[] versions = line.trim().split("."); 
+				// first part is name, second is number 
+				for(String version : versions) 
+				{
+					ArrayList<ActionItem> actionItems = new ArrayList<ActionItem>();
+					String[] parts = version.trim().split(":");
+					int versionNumber = parts[0].charAt(1);
+					String[] actionItemElements = parts[1].trim().split(",");
+					
+					int count = 0;
+					
+					for(int i = actionItemElements.length - 1; i >= 0; i--) 
+					{
+						String comments = actionItemElements[(actionItemElements.length - 1) - (count * 5)];
+						
+						String dateCompleteString = actionItemElements[(actionItemElements.length - 2) - (count * 5)];
+						String[] dateCompleteComponents = dateCompleteString.split("-");
+						LocalDateTime dateComplete = LocalDateTime.of(Integer.parseInt(dateCompleteComponents[0]), Integer.parseInt(dateCompleteComponents[1]), 
+								dateCompleteComponents[2].charAt(9) + dateCompleteComponents[2].charAt(10), 11, 59);
+						
+						String dateAddedString = actionItemElements[(actionItemElements.length - 3) - (count * 5)];
+						String[] dateAddedComponents = dateAddedString.split("-");
+						LocalDateTime dateAdded = LocalDateTime.of(Integer.parseInt(dateAddedComponents[0]), Integer.parseInt(dateAddedComponents[1]), 
+								dateAddedComponents[2].charAt(9) + dateAddedComponents[2].charAt(10), 11, 59); 
+						
+						String status = actionItemElements[(actionItemElements.length - 4) - (count * 5)];
+						
+						String name = actionItemElements[(actionItemElements.length - 5) - (count * 5)];
+						
+						ActionItem ai = new ActionItem(name, status, dateAdded, dateComplete, comments);
+						actionItems.add(ai);
+					}
+					ActionItem[] actionItemsList = new ActionItem[actionItems.size()];
+					for(int i = 0; i < actionItems.size(); i++) 
+					{
+						actionItemsList[i] = actionItems.get(i);
+					}
+					map.put(versionNumber, actionItemsList);
+				}
+			} 
+		} 
+		catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+		finally { 
+
+			// Always close the BufferedReader 
+			if (br != null) { 
+				try { 
+					br.close(); 
+				} 
+				catch (Exception e) { 
+				}; 
+			} 
+		} 
+
+		return map; 
+	} 
+}
+
