ecb9f1f647636f122a63e2d9abb9369b2fd243cd
ngangi0195 <96700425+ngangi0195@users.noreply.github.com>
2024-02-23 09:52:58 -0500

    Add files via upload

--- HashMapReadTest.java
+++ HashMapReadTest.java
@@ -1,4 +1,4 @@
-package dev;
+package smth;
 
 //Java program to reading 
 //text file to HashMap 
@@ -9,85 +9,122 @@ import java.util.Date;
 import java.time.*;
 
 public class HashMapReadTest { 
+	
+	//File path where the versions are being read from
 	final static String filePath 
-		= "test.txt"; 
+		= "text.txt"; 
+	
 	public static void main(String[] args) 
 	{ 
 		// read text file to HashMap 
 		Map<Integer, ActionItem[]> mapFromFile 
 			= HashMapFromTextFile(); 
+		
 
 		// iterate over HashMap entries 
 		for (Map.Entry<Integer, ActionItem[]> entry : 
 			mapFromFile.entrySet()) { 
-			System.out.println(entry.getKey() + " : "
-							+ entry.getValue()); 
+			System.out.println("Version" + entry.getKey() + ":");
+			for(ActionItem a : entry.getValue()) 
+			{
+				System.out.println(a.toString());
+			}
 		} 
 	} 
 
+	/*
+	 * Returns a map of the keys and according values as written in the file
+	 * */
 	public static Map<Integer, ActionItem[]> HashMapFromTextFile() 
 	{ 
-
+		
+		//Creates map that will be returned
 		Map<Integer, ActionItem[]> map 
 			= new HashMap<Integer, ActionItem[]>(); 
+			
+		//Initialize Buffered Reader to read file
 		BufferedReader br = null; 
 
 		try { 
-
 			// create file object 
 			File file = new File(filePath); 
 
 			// create BufferedReader object from the File 
 			br = new BufferedReader(new FileReader(file)); 
 
+			//reads file line by line and adds line to text
+			String text = "";
 			String line = null; 
+			while ((line = br.readLine()) != null) 
+			{
+				text += line;
+			}
 
-			// read file line by line 
-			while ((line = br.readLine()) != null) { 
-
-				// split the line by : 
-				String[] versions = line.trim().split("."); 
-				// first part is name, second is number 
+				// split the text by . to separate versions 
+				String[] versions = text.trim().split("split"); 
 				for(String version : versions) 
 				{
+					System.out.println(version);
+				}
+				
+				// iterate over each version in version history
+				for(String version : versions) 
+				{
+					//initializing where actionItems will be stored before map is created
 					ArrayList<ActionItem> actionItems = new ArrayList<ActionItem>();
-					String[] parts = version.trim().split(":");
-					int versionNumber = parts[0].charAt(1);
+					
+					//splits version ID and actionItemElements
+					String[] parts = version.trim().split(";");
+					int versionNumber = parts[0].charAt(1) - 48;
+					System.out.println("\nVersion Number:" + versionNumber);
+					
+					//splits specific elements
 					String[] actionItemElements = parts[1].trim().split(",");
 					
 					int count = 0;
 					
-					for(int i = actionItemElements.length - 1; i >= 0; i--) 
+					//traversing backwards
+					for(int i = (actionItemElements.length / 5) - 1; i >= 0; i--) 
 					{
-						String comments = actionItemElements[(actionItemElements.length - 1) - (count * 5)];
+						int newIndex = (actionItemElements.length - 1) - (count * 5);
+						String comments = actionItemElements[newIndex];
 						
-						String dateCompleteString = actionItemElements[(actionItemElements.length - 2) - (count * 5)];
+						String dateCompleteString = actionItemElements[newIndex - 1];
 						String[] dateCompleteComponents = dateCompleteString.split("-");
-						LocalDateTime dateComplete = LocalDateTime.of(Integer.parseInt(dateCompleteComponents[0]), Integer.parseInt(dateCompleteComponents[1]), 
-								dateCompleteComponents[2].charAt(9) + dateCompleteComponents[2].charAt(10), 11, 59);
 						
-						String dateAddedString = actionItemElements[(actionItemElements.length - 3) - (count * 5)];
+						int day = ((dateCompleteComponents[2].charAt(0) - 48) * 10) + (dateCompleteComponents[2].charAt(1) - 48);
+						
+						LocalDateTime dateComplete = LocalDateTime.of(Integer.parseInt(dateCompleteComponents[0]), (Integer.parseInt(dateCompleteComponents[1])), 
+								day, 11, 59);
+						
+						String dateAddedString = actionItemElements[newIndex - 2];
 						String[] dateAddedComponents = dateAddedString.split("-");
+						
+						day = ((dateAddedComponents[2].charAt(0) - 48) * 10) + (dateAddedComponents[2].charAt(1) - 48);
+
 						LocalDateTime dateAdded = LocalDateTime.of(Integer.parseInt(dateAddedComponents[0]), Integer.parseInt(dateAddedComponents[1]), 
-								dateAddedComponents[2].charAt(9) + dateAddedComponents[2].charAt(10), 11, 59); 
+								day, 11, 59); 
 						
-						String status = actionItemElements[(actionItemElements.length - 4) - (count * 5)];
+						String status = actionItemElements[newIndex - 3];
 						
-						String name = actionItemElements[(actionItemElements.length - 5) - (count * 5)];
+						String name = actionItemElements[newIndex - 4];
 						
 						ActionItem ai = new ActionItem(name, status, dateAdded, dateComplete, comments);
 						actionItems.add(ai);
+						
+						count = count + 1;
 					}
 					ActionItem[] actionItemsList = new ActionItem[actionItems.size()];
 					for(int i = 0; i < actionItems.size(); i++) 
 					{
-						actionItemsList[i] = actionItems.get(i);
+						ActionItem a = new ActionItem(actionItems.get(i).getName(), actionItems.get(i).getStatus(), actionItems.get(i).getDate(), actionItems.get(i).getDate(), actionItems.get(i).getComments());
+						actionItemsList[i] = a;
 					}
 					map.put(versionNumber, actionItemsList);
 				}
-			} 
 		} 
 		catch (Exception e) { 
+			System.out.println("Error");
 			e.printStackTrace(); 
 		} 
 		finally { 
@@ -105,4 +142,3 @@ public class HashMapReadTest {
 		return map; 
 	} 
 }
-
--- HashMapWriteTest.java
+++ HashMapWriteTest.java
@@ -1,10 +1,10 @@
-package dev;
+package smth;
 
 import java.io.*;
 import java.util.*;
 
 public class HashMapWriteTest {
-	static HashMap<Integer, ActionItem[]> map = new HashMap<>();
+	HashMap<Integer, ActionItem[]> map = new HashMap<>();
 	
 	final static String outputFilePath = "text.txt";
 	File file = new File(outputFilePath);
@@ -27,12 +27,12 @@ public class HashMapWriteTest {
   
             // iterate map entries 
             for (int i: map.keySet()) {
-            	bf.write("V" + i + ":\n");
+            	bf.write("V" + i + ";\n");
             	for(ActionItem item : map.get(i)) 
             	{
             		bf.write(item.toFile() + "\n"); 
             	}
-            	bf.write(".");
+            	bf.write("split\n");
             } 
   
             bf.flush(); 
@@ -55,11 +55,11 @@ public class HashMapWriteTest {
 	
 	public static void main(String [] args)
 	{
-		System.out.println("Hello world");
 		ActionItem[] list = {new ActionItem("Nidhin", "Urgent", 2, 5, 2024, "Doing work"), new ActionItem("Sam", "Current", 2, 4, 2024, "Not doing work"), new ActionItem("Tristan", "Awaiting", 2, 3, 2024, "Working")};
 		ActionItem[] list2 = {new ActionItem("Maddie", "Lost", 2, 6, 2024, "Playing Games"), new ActionItem("Lily", "Current", 2, 4, 2024, "Actively Working"), new ActionItem("Aiden", "Awaiting", 2, 3, 2024, "Chipotle")};
 		HashMapWriteTest h = new HashMapWriteTest();
 		h.createVersion(1, list);
 		h.createVersion(2, list2);
+		
 	}
 }
