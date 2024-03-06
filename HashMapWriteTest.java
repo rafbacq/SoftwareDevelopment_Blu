76eec6298ba3a121deee839b6e0d4416422a87ed
ngangi0195 <96700425+ngangi0195@users.noreply.github.com>
2024-03-06 10:47:14 -0500

    Add files via upload

--- HashMapWriteTest.java
+++ HashMapWriteTest.java
@@ -1,18 +1,33 @@
-package dev;
+package smth;
 
 import java.io.*;
+import java.util.Date;
 import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
 
 public class HashMapWriteTest {
-	static HashMap<Integer, ActionItem[]> map = new HashMap<>();
+	HashMap<Integer, ArrayList<ActionItem>> map = new HashMap<>();
 	
-	final static String outputFilePath = "text.txt";
+	static String outputFilePath = "version.txt";
 	File file = new File(outputFilePath);
+	public static int listNum=1;
 	
-	public void createVersion(int versionNumber, ActionItem[] list) 
+	public HashMapWriteTest()
 	{
+		map = new HashMap<>();
+		System.out.println("Map Created");
+	}
+	public static String getFilePath()
+	{
+		return outputFilePath;
+	}
+	public void createVersion(int versionNumber, ArrayList<ActionItem> list) 
+	{
+		map = new HashMap<>();
 		map.put(versionNumber, list);
-		
 		FileWriter bf = null;
 
 		try { 
@@ -24,15 +39,22 @@ public class HashMapWriteTest {
 			  
             // create new BufferedWriter for the output file 
             bf = new FileWriter(outputFilePath, true); 
+            System.out.println("Buffered Reader Created");
   
             // iterate map entries 
             for (int i: map.keySet()) {
-            	bf.write("V" + i + ":\n");
-            	for(ActionItem item : map.get(i)) 
+            	if (map != null) 
             	{
-            		bf.write(item.toFile() + "\n"); 
+            		bf.write("V" + i + " -- " + LocalDateTime.now().toString().substring(0, 10) + ";\n");
+            		System.out.println("V" + i + " -- " + LocalDateTime.now().toString().substring(0, 10) + ";\n");
+	            	for(ActionItem item : map.get(i)) 
+	            	{
+	            		bf.write(item.toFile() + "\n");
+	            		System.out.println(item.toFile() + "\n");
+	            	}
+	            	bf.write("split\n");
+	            	System.out.println("split\n");
             	}
-            	bf.write(".\n");
             } 
   
             bf.flush(); 
@@ -51,18 +73,22 @@ public class HashMapWriteTest {
             } 
         }
 		System.out.println("Complete");
-		System.out.println(file.getPath());
-		System.out.println(file.getAbsolutePath());
 	}
 	
-	public static void main(String [] args)
+	public static void main(String[] args) 
 	{
-		System.out.println("Hello world");
-		ActionItem[] list = {new ActionItem("Nidhin", "Urgent", 2, 5, 2024, "Doing work"), new ActionItem("Sam", "Current", 2, 4, 2024, "Not doing work"), new ActionItem("Tristan", "Awaiting", 2, 3, 2024, "Working")};
-		ActionItem[] list2 = {new ActionItem("Maddie", "Lost", 2, 6, 2024, "Playing Games"), new ActionItem("Lily", "Current", 2, 4, 2024, "Actively Working"), new ActionItem("Aiden", "Awaiting", 2, 3, 2024, "Chipotle")};
+		ArrayList<ActionItem> list = new ArrayList<ActionItem>();
+		list.add(new ActionItem("Nidhin", "Urgent", 2, 5, 2024, "Doing work"));
+		list.add(new ActionItem("Sam", "Current", 2, 4, 2024, "Not doing work"));
+		list.add(new ActionItem("Tristan", "Awaiting", 2, 3, 2024, "Working"));
+		ArrayList<ActionItem> list2 = new ArrayList<ActionItem>();
+		list2.add(new ActionItem("Maddie", "Lost", 2, 6, 2024, "Playing Games"));
+		list2.add(new ActionItem("Lily", "Current", 2, 4, 2024, "Actively Working"));
+		list2.add(new ActionItem("Aiden", "Awaiting", 2, 3, 2024, "Chipotle"));
 		HashMapWriteTest h = new HashMapWriteTest();
 		h.createVersion(1, list);
 		h.createVersion(2, list2);
-		
+		h.createVersion(3, list);
+		h.createVersion(4, list2);
 	}
 }
